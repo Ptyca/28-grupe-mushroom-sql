5cc6d0b38b82040b9c32c75960486c58bf510e25
Ptyca <82829609+Ptyca@users.noreply.github.com>
2021-09-06 18:38:46 +0300

    gath names

--- index.js
+++ index.js
@@ -23,10 +23,20 @@ app.init = async () => {
     for (let i = 0; i < rows.length; i++) {
         const mushroomName = rows[i].mushroom;
         const mushroomPrice = rows[i].price;
-
-        console.log(`${i + 1}) ${mushroomName} - ${mushroomPrice} EUR / kg`);
+        const mushroomNameFirstCapital = mushroomName.charAt(0).toUpperCase() + mushroomName.slice(1);
+        console.log(`${i + 1}) ${mushroomNameFirstCapital} - ${mushroomPrice} EUR / kg`);
     }
+    sql = 'SELECT `name` FROM `gatherer`';
+    [rows] = await connection.execute(sql);
 
+    let gathererNames = [];
+    for (let index = 0; index < rows.length; index++) {
+        const grybautojas = rows[index].name;
+        if (!gathererNames.includes(grybautojas)) {
+            gathererNames.push(grybautojas);
+        }
+    }
+    console.log(`Grybautojai: ${gathererNames.join(', ')}.`);
 }
 
 
